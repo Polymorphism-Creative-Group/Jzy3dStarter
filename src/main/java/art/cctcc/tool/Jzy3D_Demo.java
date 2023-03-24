03fd2ae0b761cc503c62e55ba05cd949b3bf8947
Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
2023-03-25 07:22:21 +0800

    update dependency

--- src/main/java/art/cctcc/tool/Jzy3D_Demo.java
+++ src/main/java/art/cctcc/tool/Jzy3D_Demo.java
@@ -5,16 +5,14 @@
  */
 package art.cctcc.tool;
 
-import org.jzy3d.chart.AWTChart;
-import org.jzy3d.chart.Chart;
+import org.jzy3d.chart.factories.AWTChartFactory;
 import org.jzy3d.colors.Color;
 import org.jzy3d.colors.ColorMapper;
 import org.jzy3d.colors.colormaps.ColorMapRainbow;
 import org.jzy3d.maths.Range;
-import org.jzy3d.plot3d.builder.Builder;
+import org.jzy3d.plot3d.builder.SurfaceBuilder;
 import org.jzy3d.plot3d.builder.Mapper;
 import org.jzy3d.plot3d.builder.concrete.OrthonormalGrid;
-import org.jzy3d.plot3d.primitives.Shape;
 import org.jzy3d.plot3d.rendering.canvas.Quality;
 
 /**
@@ -23,31 +21,31 @@ import org.jzy3d.plot3d.rendering.canvas.Quality;
  */
 public class Jzy3D_Demo {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        // Define a function to plot
-        Mapper mapper = new Mapper() {
-            public double f(double x, double y) {
-                return 10 * Math.sin(x / 10) * Math.cos(y / 20);
-            }
-        };
+    // Define a function to plot
+    var mapper = new Mapper() {
+      @Override
+      public double f(double x, double y) {
+        return 10 * Math.sin(x / 10) * Math.cos(y / 20);
+      }
+    };
 
-        // Define range and precision for the function to plot
-        Range range = new Range(-150, 150);
-        int steps = 50;
+    // Define range and precision for the function to plot
+    var range = new Range(-150, 150);
+    var steps = 50;
 
-        // Create a surface drawing that function
-        Shape surface = Builder.buildOrthonormal(new OrthonormalGrid(range, steps), mapper);
-        surface.setColorMapper(
-                new ColorMapper(
-                        new ColorMapRainbow(), surface.getBounds().getZRange()));
-        surface.setFaceDisplayed(true);
-        surface.setWireframeDisplayed(false);
-        surface.setWireframeColor(Color.BLACK);
+    // Create a surface drawing that function
+    var surface = new SurfaceBuilder().orthonormal(new OrthonormalGrid(range, steps), mapper);
+    System.out.println(surface.getBounds().getZRange());
+    surface.setColorMapper(new ColorMapper(new ColorMapRainbow(), surface.getBounds().getZRange()));
+    surface.setFaceDisplayed(true);
+    surface.setWireframeDisplayed(false);
+    surface.setWireframeColor(Color.BLACK);
 
-        // Create a chart and add the surface
-        Chart chart = new AWTChart(Quality.Advanced);
-        chart.add(surface);
-        chart.open("Jzy3d Demo", 600, 600);
-    }
+    // Create a chart and add the surface
+    var chart = AWTChartFactory.chart(Quality.Advanced());
+    chart.add(surface);
+    chart.open("Jzy3d Demo", 600, 600);
+  }
 }
